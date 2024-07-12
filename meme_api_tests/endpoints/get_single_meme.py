771d51f25f06b4f5cf3baf7b2b605f9b8565dc86
Dozer <shitov.ea@gmail.com>
2024-07-12 05:54:17 +0400

    Add schema check for get_single_meme.py

--- meme_api_tests/endpoints/get_single_meme.py
+++ meme_api_tests/endpoints/get_single_meme.py
@@ -1,19 +1,23 @@
 import allure
 import requests
 
-from meme_api_tests.client import MEME_URL, attach_response
+from meme_api_tests.client import MEME_URL, attach_response, validate_response
 
 from meme_api_tests.endpoints.base_api import BaseApi
+
+from meme_api_tests.endpoints.schemas import ResponseSchema
 
 
 class GetMeme(BaseApi):
     @allure.step('Get object')
     def get_meme(self, meme_id: int, token: str) -> None:
-        headers = {"Authorization": token}
+        headers: dict = {"Authorization": token}
         self.response: requests.Response = requests.get(f"{MEME_URL}/{meme_id}", headers=headers)
         # attach_response(self.response, "Response")
-        self.response_json: dict = self.response.json()
+        response_json: dict = self.response.json()
+
+        validate_response(self, response_json, "Response", ResponseSchema)
 
     @allure.step('Check response id')
     def check_response_id_is_(self, meme_id) -> tuple[bool, str]:
-        return self.response_json['id'] == meme_id, f"Expected id {meme_id}, got {self.response_json['id']}"
+        return self.valid_response.id == meme_id, f"Expected id {meme_id}, got {self.valid_response.id}"
